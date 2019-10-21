102879204045aeaf4ba5a82989e68e4bbf982f30
BlerinaP <blerina1.p@gmail.com>
2019-10-21 13:54:22 +0200

    Showing photos in front end of the page Gallery

--- themes/gymfitnesswptheme/gallery.php
+++ themes/gymfitnesswptheme/gallery.php
@@ -8,7 +8,20 @@ get_header();?>
     <?php while(have_posts() ): the_post(); ?>
     <h1 class="text-center text-primary"><?php the_title();?></h1>
 
-        <?php the_content();?>
+       <?php
+         $gallery = get_post_gallery(get_the_ID(), false );
+         $gallery_images_ids = explode(',', $gallery['ids']);
+        ?>
+
+    <ul class="gallery-images">
+        <?php
+           foreach($gallery_images_ids as $id):
+           $image = wp_get_attachment_image_src($id, 'square');?>
+           <img src="<?php echo $image[0]; ?>"/>
+
+         <?php endforeach; ?>
+    </ul>
+
     <?php endwhile; ?>
 </main>
 
