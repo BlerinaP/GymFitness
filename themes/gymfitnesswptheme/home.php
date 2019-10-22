fcdd4279731f3b8c7e77aa9ed03d24774bc7713f
BlerinaP <blerina1.p@gmail.com>
2019-10-22 11:24:44 +0200

    Adding CSS for blog posts.

--- themes/gymfitnesswptheme/home.php
+++ themes/gymfitnesswptheme/home.php
@@ -14,7 +14,8 @@
                          <?php echo get_the_author_meta('display_name'); ?>
                     </a>
                    </p>
-                   <p class="date-published">
+                   <p class="date-published meta">
+                       <span>Date:</span>
                       <?php the_time(get_option('date_format'));?>
                    </p>
                </div>
