23f87d1fa06f48cbe9e12cc5540d8592a28d67a5
BlerinaP <blerina1.p@gmail.com>
2019-10-22 12:31:33 +0200

    Adding the Category Template

--- themes/gymfitnesswptheme/functions.php
+++ themes/gymfitnesswptheme/functions.php
@@ -54,11 +54,11 @@ function gymfitness_scripts(){
 
 function gymfitness_setup(){
      //Register new image sizze
-    add_image_size('square',350,350,true);
-    add_image_size('portrait',350,724,true);
-    add_image_size('box',400,375,true);
-    add_image_size('mediumSize',700,400,true);
-    add_image_size('blog',966,655,true);
+    add_image_size('square', 350, 350, true);
+    add_image_size('portrait', 350, 724, true);
+    add_image_size('box', 400, 375, true);
+    add_image_size('mediumSize', 700, 400, true );
+    add_image_size('blog', 966, 644, true);
      //Add featured image
     add_theme_support('post-thumbnails');
 }
