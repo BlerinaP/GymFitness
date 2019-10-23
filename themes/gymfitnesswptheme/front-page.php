28d18aad00f33e01bc4862ca54509158c8bb50d3
BlerinaP <blerina1.p@gmail.com>
2019-10-23 14:34:46 +0200

    Adding boxes for the pictures and displaying values (name,image)

--- themes/gymfitnesswptheme/front-page.php
+++ themes/gymfitnesswptheme/front-page.php
@@ -6,6 +6,42 @@
      <p><?php the_field('welcome_text');?></p>
  </section>
 
+ <section class="section-areas">
+   <ul class="areas-container">
+       <li class="area">
+           <?php
+           $area1 = get_field('area_1');
+           $image = wp_get_attachment_image_src($area1['area_image'],'mediumSize')[0];
+           ?>
+           <img src="<?php echo $image ?>" />
+           <p><?php echo $area1['area_name'];?></p>
+       </li>
+       <li class="area">
+           <?php
+           $area2 = get_field('area_2');
+           $image = wp_get_attachment_image_src($area2['area_image'],'mediumSize')[0];
+           ?>
+           <img src="<?php echo $image ?>" />
+           <p><?php echo $area2['area_name'];?></p>
+       </li>
+       <li class="area">
+           <?php
+           $area3 = get_field('area_3');
+           $image = wp_get_attachment_image_src($area3['area_image'],'mediumSize')[0];
+           ?>
+           <img src="<?php echo $image ?>" />
+           <p><?php echo $area3['area_name'];?></p>
+       </li>
+       <li class="area">
+           <?php
+           $area4 = get_field('area_4');
+           $image = wp_get_attachment_image_src($area4['area_image'],'mediumSize')[0];
+           ?>
+           <img src="<?php echo $image ?>" />
+           <p><?php echo $area4['area_name'];?></p>
+       </li>
+   </ul>
+ </section>
 
 
 <?php endwhile; ?>
