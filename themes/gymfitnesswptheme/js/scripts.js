178c462c3f37393106bd28bfa248a0120dbbbf0a
BlerinaP <blerina1.p@gmail.com>
2019-10-25 10:53:41 +0200

    Adding an animation to the main nav when scrolling

--- themes/gymfitnesswptheme/js/scripts.js
+++ themes/gymfitnesswptheme/js/scripts.js
@@ -9,4 +9,30 @@
           controls: false,
           mode: 'horizontal'
       });
+
+       //When the page is ready add the fixed menu if position is grater than 300px
+     const headerScroll = document.querySelector('.navigation-bar');
+     const rect = headerScroll.getBoundingClientRect();
+     const topDistance = Math.abs(rect.top);
+     fixedMenu(topDistance);
 });
+
+//Scrolling animation
+
+ window.onscroll = () => {
+     const scroll = window.scrollY;
+     fixedMenu(scroll);
+ }
+
+ // Adds a fixed menu on top
+
+ function fixedMenu(scroll){
+     const headerScroll = document.querySelector('.navigation-bar');
+
+     //In case that the scroll is greater than 300px add some classes
+     if(scroll > 300){
+         headerScroll.classList.add('fixed-top');
+     } else{
+         headerScroll.classList.remove('fixed-top');
+     }
+ }
